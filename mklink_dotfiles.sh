0e464ddb1cb9c283d7b201089fd84a93e5c76d12
gelegele <ryo500209@gmail.com>
2013-05-23 17:40:46 +0900

    new sh to make symlinks of dotfiles

---
+++ mklink_dotfiles.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "No args. Specify a source directory."
+  exit 1
+elif [ $# -gt 2 ]; then
+  echo "Too many args."
+  exit 1
+fi
+
+if [ ! -d $1 ]; then
+  echo "$1 is not a directory."
+  exit 1
+fi
+src=$1
+
+dst=./
+if [ $# -eq 2 ]; then
+  if [ ! -d $2 ]; then
+    echo "$2 is not a directory."
+    exit 1
+  fi
+  dst=$2
+fi
+
+
+for file in `ls -a $src`; do
+  if [ $file = "." -o $file = ".." -o $file = ".git" ]; then
+    continue
+  fi
+	lncmd="ln -s $src$file $dst$file"
+  echo $lncmd
+	$lncmd
+done
