0bd7a2cc2c4f7bd2c8aceda0aebdfeddf8a7d661
gelegele <ryo500209@gmail.com>
2023-04-03 17:16:31 +0900

    Install pip3

--- init-scripts/01_apt_install.sh
+++ init-scripts/01_apt_install.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y zsh vim language-pack-ja git tig ssh source-highlight tree unzip
 
-packages=(fzf ipython3)
+packages=(fzf python3-pip ipython3)
 for p in "${packages[@]}"
 do
   read -p "Do you install ${p}? [y/n]: " yn
