ae40f693707dff9ac01f3c65bedba78719ebbfb2
gelegele <ryo500209@gmail.com>
2023-04-14 05:50:19 +0900

    nvimlsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -248,16 +248,78 @@ require('lazy').setup({
         return
       end
       require'nvim-treesitter.configs'.setup {
-        ensure_installed = { "bash", "python", "lua", "html", "dockerfile", "javascript" },
+        ensure_installed = {
+          "bash", "css", "diff", "dockerfile", "gitcommit", "git_rebase", "html",
+          "javascript", "json", "lua", "make", "passwd", "php", "python", "ruby", "sql", "yaml",
+        },
         auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
+        highlight = { enable = true, },
+        indent = { enable = true, },
       }
     end
+  },
+  { -- Show indents
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'BufRead',
+  },
+  {
+    'neovim/nvim-lspconfig',
+  },
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup_handlers({ function(server)
+        local opt = {
+          -- -- Function executed when the LSP server startup
+          -- on_attach = function(client, bufnr)
+          --   local opts = { noremap=true, silent=true }
+          --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+          -- end,
+          capabilities = require('cmp_nvim_lsp').update_capabilities(
+            vim.lsp.protocol.make_client_capabilities()
+          )
+        }
+        require('lspconfig')[server].setup(opt)
+      end })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          -- { name = "buffer" },
+          -- { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ['<C-l>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm { select = true },
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
   },
   {
     'folke/which-key.nvim',
