c67e3d33a419a250323f7903c9741893d381397a
gelegele <ryo500209@gmail.com>
2023-04-23 03:29:16 +0900

    wsl-open and gh installer

---
+++ init-scripts/202_wsl-open.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$(uname -r)" != *microsoft* ]]; then
+  echo 'You are not on WSL. No need this.'
+  exit
+fi
+
+npm install -g wsl-open
+sudo ln -s $(which wsl-open) /usr/local/bin/xdg-open
