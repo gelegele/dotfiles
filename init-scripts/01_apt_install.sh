726f8a0ffc641e3ce9acad0487464cfb87af43cb
gelegele <ryo500209@gmail.com>
2023-04-23 00:36:44 +0900

    install curl

--- init-scripts/01_apt_install.sh
+++ init-scripts/01_apt_install.sh
@@ -2,9 +2,9 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y zsh vim language-pack-ja git tig ssh source-highlight tree unzip
+sudo apt install -y curl vim git tig ssh source-highlight tree unzip
 
-packages=(fzf python3-pip ipython3)
+packages=(language-pack-ja python3-pip ipython3)
 for p in "${packages[@]}"
 do
   read -p "Do you install ${p}? [y/n]: " yn
--- init-scripts/03_zsh.sh
+++ init-scripts/03_zsh.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo apt update -y
+sudo apt install -y zsh
+
+# Change default shell zsh.
+chsh -s $(which zsh)
+
 rm -rf ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 mkdir -p ~/.zsh/completion
 curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker -o ~/.zsh/completion/_docker
 curl -L https://raw.githubusercontent.com/docker/compose/v2.4.1/contrib/completion/zsh/_docker-compose -o ~/.zsh/completion/_docker-compose
-# .zshrc にて fpath=(~/.zsh/completion $fpath) 
