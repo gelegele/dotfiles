3d16b4c55874b139316cd8cf8d905bafb89688d2
gelegele <ryo500209@gmail.com>
2023-04-30 00:48:57 +0900

    mklink_config_files.sh interactive mode.

--- init-scripts/102_mklink_config_files.sh
+++ init-scripts/102_mklink_config_files.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-
 # Make .config symlinks
+
+read -p "Do you want to setup interactively? (y or else): " ans
+if [[ $ans = 'y' ]]; then
+  mode=interactive
+fi
 
 confdir=$(cd $(dirname $0)/../.config;pwd)
 
 for appdir in `ls -A $confdir`; do
+
+  if [[ $mode = interactive ]]; then
+    # Interactive mode
+    read -p "Do you setup ${appdir}? (y or else): " ans
+    if [[ $ans != 'y' ]]; then
+      echo "Skip ${appdir} setup."
+      continue
+    fi
+  fi
 
   srcdir=$confdir/$appdir/
   dstdir=$XDG_CONFIG_HOME/$appdir/
