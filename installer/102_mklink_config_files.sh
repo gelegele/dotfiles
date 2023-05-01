dd2006f1a562e5fc6e8429a7cc825dba7f4dcfc7
gelegele <ryo500209@gmail.com>
2023-05-01 18:54:42 +0900

    refactoring bash if and or

--- installer/102_mklink_config_files.sh
+++ installer/102_mklink_config_files.sh
@@ -10,7 +10,7 @@ fi
 homesrc=$(cd $(dirname $0)/..;pwd)
 homedst=~
 for file in `ls -AF $homesrc`; do
-  if [[ -d $file ]] || [[ $file != .* ]]; then
+  if [[ -d $file || $file != .* ]]; then
     # Skip directories and normal files.
     continue
   fi
