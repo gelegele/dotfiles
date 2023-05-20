d326d960a87fc5731d684b1892ae6f729e17742c
gelegele <ryo500209@gmail.com>
2023-05-20 10:14:36 +0900

    cmp config bug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -337,6 +337,21 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
   },
   {
+    'hrsh7th/cmp-buffer',
+  },
+  {
+    'hrsh7th/cmp-path',
+  },
+  {
+    'hrsh7th/cmp-cmdline',
+  },
+  {
+    'L3MON4D3/LuaSnip',
+  },
+  {
+    'saadparwaiz1/cmp_luasnip',
+  },
+  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     config = function()
@@ -344,11 +359,12 @@ require('lazy').setup({
       cmp.setup({
         snippet = {
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
         sources = {
           { name = "nvim_lsp" },
+          { name = 'luasnip' },
           -- { name = "buffer" },
           -- { name = "path" },
         },
