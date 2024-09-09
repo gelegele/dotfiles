e2082b2a4ca1bd7eb5658ecd9baff3fe5bb82353
gelegele <ryo500209@gmail.com>
2024-09-09 13:17:22 +0900

    kaizen nvim noice message config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -604,6 +604,13 @@ require('lazy').setup({
         return
       end
       require("noice").setup({
+        messages = {
+          view = "mini",
+          view_error = "mini", -- view for errors
+          view_warn = "mini", -- view for warnings
+          view_history = "mini", -- view for :messages
+          view_search = "mini", -- view for search count messages. Set to `false` to disable
+        },
         lsp = {
           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
