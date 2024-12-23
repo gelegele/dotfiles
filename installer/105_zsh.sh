4ed0fb1ceb6fe2624d11a9739944bd1c891fa83e
gelegele <ryo500209@gmail.com>
2024-12-23 14:23:45 +0900

    Fix XDG_CONFIG_HOME bug

--- installer/105_zsh.sh
+++ installer/105_zsh.sh
@@ -8,10 +8,10 @@ sudo apt install -y zsh
 $(dirname $0)/sub_mklink_config.sh zsh
 
 # Install plugin manager
-git clone --depth 1 https://github.com/zsh-users/antigen.git $XDG_CONFIG_HOME/zsh/antigen/
+git clone --depth 1 https://github.com/zsh-users/antigen.git $HOME/.config/zsh/antigen/
 
 mkdir -p ~/.config/zsh/completion
-curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker -o $XDG_CONFIG_HOME/zsh/completion/_docker
+curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker -o $HOME/.config/zsh/completion/_docker
 
 echo 'Change default shell zsh.'
 chsh -s $(which zsh)
--- installer/108_vifm.sh
+++ installer/108_vifm.sh
@@ -8,5 +8,5 @@ sudo apt install -y vifm
 $(dirname $0)/sub_mklink_config.sh vifm
 
 # to show icons
-curl -m 1 --retry 3 https://raw.githubusercontent.com/thimc/vifm_devicons/master/favicons.vifm -o $XDG_CONFIG_HOME/vifm/favicons.vifm
+curl -m 1 --retry 3 https://raw.githubusercontent.com/thimc/vifm_devicons/master/favicons.vifm -o $HOME/.config/vifm/favicons.vifm
 
