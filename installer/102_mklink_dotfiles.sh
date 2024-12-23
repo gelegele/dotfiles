732c97d4e49a95a2ea6570a53b271f4e1dd06ce6
gelegele <ryo500209@gmail.com>
2024-12-23 13:49:45 +0900

    Fix mklink bug

--- installer/102_mklink_dotfiles.sh
+++ installer/102_mklink_dotfiles.sh
@@ -3,7 +3,7 @@
 # make symlinks of dotfiles in HOME
 homesrc=$(cd $(dirname $0)/../home;pwd)
 homedst=~
-for file in `ls -AF $homesrc`; do
+for file in `ls -Ap $homesrc`; do
   if [[ $file == */ || $file != .* ]]; then
     # Skip directories and normal files.
     continue
