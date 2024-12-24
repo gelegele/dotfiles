3048b996f607fa89272b8855da9344dc6143aa53
gelegele <ryo500209@gmail.com>
2024-12-24 09:18:55 +0900

    Remind to install tmux plugins

--- installer/111_tmux.sh
+++ installer/111_tmux.sh
@@ -10,3 +10,7 @@ $(dirname $0)/sub_mklink_config.sh tmux
 # install tmux plugin manager
 git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
+# Remind message
+echo ""
+echo "Don't forget to install tmux plugins by pressing 'prefix + I' in tmux."
+read -p "Press any key to continue..."
