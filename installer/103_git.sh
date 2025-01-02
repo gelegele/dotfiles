4f5355133f4eabaf4077b06f456f74423b19089a
gelegele <ryo500209@gmail.com>
2025-01-02 12:57:31 +0900

    install git-delta for git pager

--- installer/103_git.sh
+++ installer/103_git.sh
@@ -3,7 +3,8 @@
 echo ''
 echo 'Install git ... '
 
-sudo apt install -y git
+sudo apt install -y git git-delta
+# git-delta is a viewer for git and diff output
 
 $(dirname $0)/sub_mklink_config.sh git
 
