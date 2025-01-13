b18ccbba3ebd9c0da08af9df7674fe4cbd36114f
gelegele <ryo500209@gmail.com>
2025-01-13 20:57:09 +0900

    Install zsh by apt

--- installer/105_zsh.sh
+++ installer/105_zsh.sh
@@ -3,7 +3,13 @@
 echo ''
 echo 'Install zsh ... '
 
-brew install zsh
+
+if command -v apt-get > /dev/null 2>&1; then
+  # Coudn't Change default shell to zsh installed by Homebrew on Debian
+  sudo apt install -y zsh
+else
+  brew install zsh
+fi
 
 $(dirname $0)/sub_mklink_config.sh zsh
 
