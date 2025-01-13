e13bf12b91ac1028c9c7aa45b4ba05628fb99d4d
gelegele <ryo500209@gmail.com>
2025-01-13 21:49:47 +0900

    Excluded git from brew

--- installer/103_git-delta.sh
+++ installer/103_git-delta.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo ''
-echo 'Install git ... '
-
-brew install git git-delta
+echo 'Install git-delta ... '
+brew install git-delta
 # git-delta is a viewer for git and diff output
 
 $(dirname $0)/sub_mklink_config.sh git
