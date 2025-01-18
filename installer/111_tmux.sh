c5a6eee97046f2ab8d1ba0b9063402c43918cc5a
gelegele <ryo500209@gmail.com>
2025-01-18 17:52:27 +0900

    Fix tpm installer bug

--- installer/111_tmux.sh
+++ installer/111_tmux.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 echo ''
-echo 'Installing tmux and tmux-plugins/tpm ...'
+echo 'Installing tmux ...'
 
-brew install tmux  tpm
+brew install tmux
 
 $(dirname $0)/sub_mklink_config.sh tmux
 
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+echo 'Installing tmux-plugins/tpm ...'
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 # Remind message
 echo ""
