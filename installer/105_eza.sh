862aed08e33a4999f6c2b4d4e32cf285c5f7194b
gelegele <ryo500209@gmail.com>
2025-01-26 07:26:16 +0900

    Refactoring eza installer

--- installer/105_eza.sh
+++ installer/105_eza.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Installing eza..."
 brew install eza
 
-source $(dirname $0)/../home/.zshenv
-
 # Configure theme in XDG_CONFIG_HOME
-rm -rf $XDG_CONFIG_HOME/eza
-mkdir -p $XDG_CONFIG_HOME/eza
-cd $XDG_CONFIG_HOME/eza
-git clone --depth 1 https://github.com/eza-community/eza-themes.git
-ln -sf $XDG_CONFIG_HOME/eza/eza-themes/themes/frosty.yml $XDG_CONFIG_HOME/eza/theme.yml
+source $(dirname $0)/../home/.zshenv
+ezadir=$XDG_CONFIG_HOME/eza
+rm -rf $ezadir
+mkdir -p $ezadir
+git clone --depth 1 https://github.com/eza-community/eza-themes.git $ezadir/eza-themes
+ln -sf $ezadir/eza-themes/themes/frosty.yml $ezadir/theme.yml
 
