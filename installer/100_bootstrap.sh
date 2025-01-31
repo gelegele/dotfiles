f3590565f4c7e3b6b414909e2c0c28e6de99874c
gelegele <ryo500209@gmail.com>
2025-01-31 15:17:22 +0900

    Refactoring brew installer

--- installer/100_bootstrap.sh
+++ installer/100_bootstrap.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-# For Debian and Ubuntu
-if type apt-get &> /dev/null; then
-  sudo apt update -y
-  sudo apt upgrade -y
-  sudo apt install -y build-essential procps curl file git
-  if  [ -e /etc/lsb-release ]; then
-    sudo apt install -y language-pack-ja
-  else
-    sudo apt install -y locales
-  fi 
-fi
-
-# Install Homebrew and set PATH temporary if Linux
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-case $OSTYPE in
-  darwin*)  #Mac
-    ;;
-  linux*)   #Linux
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    ;;
-esac
-
 # Execute 1xx_xxx.sh
 dir=$(dirname $0)
 echo "Execute 1xx_xxx.sh files in $dir folder."
--- installer/101_brew_basic_apps.sh
+++ installer/101_brew_basic_apps.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
+# For Debian and Ubuntu
+if type apt-get &> /dev/null; then
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install -y build-essential procps curl file git
+  if  [ -e /etc/lsb-release ]; then
+    sudo apt install -y language-pack-ja
+  else
+    sudo apt install -y locales
+  fi 
+fi
+
+echo "Install Homebrew and set PATH temporary if Linux"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+case $OSTYPE in
+  darwin*)  #Mac
+    ;;
+  linux*)   #Linux
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    ;;
+esac
+
+echo "Installing basic apps..."
 defaultpackages=(gcc wget tree unzip ripgrep fzf zoxide bat gh awscli)
 if [[ "$(uname -r)" != *microsoft* ]]; then
   # not WSL
   defaultpackages+=(font-hackgen font-hackgen-nerd)
 fi
-
-echo "Install $defaultpackages"
 brew install ${defaultpackages[@]}
 
