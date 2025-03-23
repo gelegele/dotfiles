cdddebd97b9992ac2d861d4b2366704b75270168
gelegele <ryo500209@gmail.com>
2025-03-23 10:02:42 +0900

    Enable sdkman auto env

--- installer/303_sdkman.sh
+++ installer/303_sdkman.sh
@@ -3,5 +3,13 @@
 echo "Installing sdkman..."
 curl -s "https://get.sdkman.io" | bash
 
+# To switch the current jdk as specified in the directory's .sdkmanrc
+sed -i 's/sdkman_auto_env=false/sdkman_auto_env=true/g' ~/.config/sdkman/etc/config
+if [ $? -eq 0 ]; then
+  echo "Enabled auto env."
+else
+  echo "Error! Check sdkman config."
+fi
+
 echo "Relogin to use sdkman"
 
