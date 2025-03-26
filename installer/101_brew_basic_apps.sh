bf1890e3d7694b2991cedd7a1547b60547ec65b1
gelegele <ryo500209@gmail.com>
2025-03-26 14:12:18 +0900

    brew install fd

--- installer/101_brew_basic_apps.sh
+++ installer/101_brew_basic_apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-defaultpackages=(gcc wget htop tree zip unzip ripgrep fzf zoxide bat nvm gh awscli)
+defaultpackages=(gcc wget htop tree zip unzip ripgrep fd fzf zoxide bat nvm gh awscli)
 if [[ "$(uname -r)" != *microsoft* ]]; then
   # not WSL
   defaultpackages+=(font-hackgen font-hackgen-nerd)
---
+++ installer/108_git-delta.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo ''
+echo 'Install fd ... '
+brew install fd
+
