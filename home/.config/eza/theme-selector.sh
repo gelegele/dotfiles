6b10fd27a3dc3ae064b01a359c01961a6c36ea06
gelegele <ryo500209@gmail.com>
2026-09-21 18:06:22 +0900

    refactor(theme-selector): improve theme loading logic and enhance error handling for missing themes

--- home/.config/eza/theme-selector.sh
+++ home/.config/eza/theme-selector.sh
@@ -3,20 +3,30 @@
 source ~/.zshenv
 ezaconfigdir=$XDG_CONFIG_HOME/eza
 
-if [ ! -d $ezaconfigdir/eza-themes ]; then
-  git clone --depth 1 https://github.com/eza-community/eza-themes.git $ezaconfigdir/eza-themes
+if [ ! -d "$ezaconfigdir/eza-themes" ]; then
+  git clone --depth 1 https://github.com/eza-community/eza-themes.git "$ezaconfigdir/eza-themes"
+fi
+
+shopt -s nullglob
+themes=()
+for themefile in "$ezaconfigdir/eza-themes/themes"/*; do
+  themes+=("$(basename "$themefile")")
+done
+
+if (( ${#themes[@]} == 0 )); then
+  echo "No eza themes found in $ezaconfigdir/eza-themes/themes" >&2
+  exit 1
 fi
 
 PS3="Select an eza theme > "
-select themefile in $(ls $ezaconfigdir/eza-themes/themes/)
+select themefile in "${themes[@]}"
 do
   if [ -n "$themefile" ]; then
-    break;
+    break
   fi
   echo "Select a number in the list."
 done
 
-ln -sf $ezaconfigdir/eza-themes/themes/$themefile $ezaconfigdir/theme.yml
+ln -sf "$ezaconfigdir/eza-themes/themes/$themefile" "$ezaconfigdir/theme.yml"
 echo "eza themefile changed $themefile"
 eza -l
-
