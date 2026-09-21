bd29d0c1935b248edd46be8bf150e889dc481389
gelegele <ryo500209@gmail.com>
2026-09-21 17:22:32 +0900

    fix(sdkman): update installation script to use SDKMAN_DIR for configuration and improve error handling

--- installer/303_sdkman.sh
+++ installer/303_sdkman.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Installing sdkman..."
+
+# Must match home/.config/zsh/.zshrc and home/.bashrc
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export SDKMAN_DIR="$XDG_CONFIG_HOME/sdkman"
+
 curl -s "https://get.sdkman.io" | bash
 
 # To switch the current jdk as specified in the directory's .sdkmanrc
-sed -i 's/sdkman_auto_env=false/sdkman_auto_env=true/g' ~/.config/sdkman/etc/config
-if [ $? -eq 0 ]; then
+if sed -i 's/sdkman_auto_env=false/sdkman_auto_env=true/g' "$SDKMAN_DIR/etc/config"; then
   echo "Enabled auto env."
 else
-  echo "Error! Check sdkman config."
+  echo "Error! Check sdkman config at $SDKMAN_DIR/etc/config." 1>&2
+  exit 1
 fi
 
 echo "Relogin to use sdkman"
-
