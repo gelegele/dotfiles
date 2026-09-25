5643e68c2fdad5ff729f2c84855b6b5275a6df35
gelegele <ryo500209@gmail.com>
2026-09-25 09:09:42 +0900

    refactor(installer): update WSL installation script to create symbolic links for wsl-open and xdg-open in Homebrew bin directory

--- installer/201_for_wsl.sh
+++ installer/201_for_wsl.sh
@@ -30,5 +30,7 @@ $(dirname $0)/sub_mklink_config.sh win32yank
 # Open url by Windows Browser.
 . "$(dirname "$0")/sub_load_nvm.sh"
 npm install -g wsl-open
-sudo ln -sf $(which wsl-open) /usr/local/bin/xdg-open
+brew_bin="$(brew --prefix)/bin"
+ln -sf "$(command -v wsl-open)" "$brew_bin/wsl-open"
+ln -sf "$(command -v wsl-open)" "$brew_bin/xdg-open"
 
