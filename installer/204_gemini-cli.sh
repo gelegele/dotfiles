135153d369dd6630bd0d0156084b73e8290d4fa7
gelegele <ryo500209@gmail.com>
2026-09-26 10:56:14 +0900

    change Gemini-CLI to Antigravity-CLI

--- installer/204_gemini-cli.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-. "$(dirname "$0")/sub_load_nvm.sh"
-
-echo "Installing @google/gemini-cli..."
-npm install -g @google/gemini-cli
-
-echo "Done. gemini $(gemini --version)"
---
+++ installer/207_antigravity_cli.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo ''
+echo 'Install Antigravity CLI ...'
+
+brew install --cask antigravity-cli
